3bb4736285f8327cec1f9523f5d26a934f1308d6
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2021-05-16 19:30:00 +0200

    tweaks

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const countdown = () => {
-  const countDate = new Date("May 14, 2022 00:00:00").getTime();
-  const now = new Date().getTime();
-  const gap = countDate - now;
+"use strict";
 
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const textDay = Math.floor(gap / day);
-  const textHour = Math.floor((gap % day) / hour)
+var countdown = function countdown() {
+  var countDate = new Date("May 14, 2022 00:00:00").getTime();
+  var now = new Date().getTime();
+  var gap = countDate - now;
+  var second = 1000;
+  var minute = second * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+  var textDay = Math.floor(gap / day);
+  var textHour = Math.floor((gap % day) / hour)
     .toString()
     .padStart(2, "0");
-  const textMinute = Math.floor((gap % hour) / minute)
+  var textMinute = Math.floor((gap % hour) / minute)
     .toString()
     .padStart(2, "0");
-  const textSecond = Math.floor((gap % minute) / second)
+  var textSecond = Math.floor((gap % minute) / second)
     .toString()
     .padStart(2, "0");
-
   document.querySelector(".primary-section-countdown-day").innerText = textDay;
   document.querySelector(".primary-section-countdown-hour").innerText = textHour;
   document.querySelector(".primary-section-countdown-min").innerText = textMinute;
   document.querySelector(".primary-section-countdown-sec").innerText = textSecond;
-
   if (gap <= 1) {
     document.querySelector(".primary-section-countdown").innerText = "Landing soon 🛬";
   }
